c23561a14fe1b799dd5415b3a9a02a4c2685a672
azimabdulshaikh <hisbullahazim@gmail.com>
2023-03-18 14:49:29 +0530

    build-v1

---
+++ build/precache-manifest.43d4f8ef7e64a14192d35aebc1350323.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9560eb97f4402bb7c935940c6c10cce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "867e428581d543259cbd",
+    "url": "/static/css/2.56e25c79.chunk.css"
+  },
+  {
+    "revision": "9375055f7ead803e937d",
+    "url": "/static/css/main.1c8a140a.chunk.css"
+  },
+  {
+    "revision": "867e428581d543259cbd",
+    "url": "/static/js/2.63eb8bbc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.63eb8bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9375055f7ead803e937d",
+    "url": "/static/js/main.555ce34e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  },
+  {
+    "revision": "5267ef19207844bf562a834fe7a24ab4",
+    "url": "/static/media/profile.5267ef19.jpg"
+  }
+]);
