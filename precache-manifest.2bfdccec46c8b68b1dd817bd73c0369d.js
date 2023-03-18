479db2e33ae2a555517d13637bb81d85b489fc4f
azimabdulshaikh <hisbullahazim@gmail.com>
2023-03-18 14:55:44 +0530

    Updates

---
+++ precache-manifest.2bfdccec46c8b68b1dd817bd73c0369d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347872bebeca991ece8446f098acd930",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "867e428581d543259cbd",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/static/css/2.56e25c79.chunk.css"
+  },
+  {
+    "revision": "221fcedbff876e137bd3",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/static/css/main.1c8a140a.chunk.css"
+  },
+  {
+    "revision": "867e428581d543259cbd",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/static/js/2.63eb8bbc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/static/js/2.63eb8bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221fcedbff876e137bd3",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/static/js/main.e512efea.chunk.js"
+  },
+  {
+    "revision": "b03d8d533d83967c78cb",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/static/js/runtime-main.50ab18ca.js"
+  },
+  {
+    "revision": "5267ef19207844bf562a834fe7a24ab4",
+    "url": "/azimabdulshaikh/react-frontend-dev-portfolio/static/media/profile.5267ef19.jpg"
+  }
+]);
